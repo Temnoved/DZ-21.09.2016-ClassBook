8a390059d41db27e20578af1dd1ffad1231761ea
Temnoved <temnoved@yandex.ru>
2016-09-24 00:25:38 +0300

    Add files via upload
    StartDot

---
+++ Book.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZClassBook
+{
+    struct Author//структура с данными автора
+    {
+        public String strSurname;
+        public String strFirstname;
+
+        public Author(String strSur = "", String strFirst = "")
+        {
+            strSurname = strSur;
+            strFirstname = strFirst;
+        }
+    };
+
+    class Book
+    {
+        ///////////////////////////////////////////////////////////////////////////////////////////////////
+        /////////////////////////////////////// поля //////////////////////////////////////////////////////
+        ///////////////////////////////////////////////////////////////////////////////////////////////////
+
+        protected Author bookAuthor = new Author();
+        protected String strTitle = "";
+        protected float fCost = 0f;
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////
+        /////////////////////////////////////// методы ////////////////////////////////////////////////////
+        ///////////////////////////////////////////////////////////////////////////////////////////////////
+
+        //конструктор с параметрами по умолчанию
+        public Book(Author auAu = new Author(), String strTit = "", float fCos = 0f)
+        {
+            this.bookAuthor = auAu;
+            this.strTitle = strTit;
+            this.fCost = fCos;
+        }
+
+        public String AuthorFirstname
+        {
+            set { this.bookAuthor.strFirstname = value; }//установка имени автора
+        }
+
+        public String AuthorSurname
+        {
+            set { this.bookAuthor.strSurname = value; }//установка фамилии автора
+        }
+
+        public Author AuthorData
+        {
+            set { this.bookAuthor = value; }
+        }
+
+        public String BookTitle
+        {
+            set { this.strTitle = value; }//установка названия книги
+        }
+
+        public float BookCost
+        {
+            set { this.fCost = (float)value; }//установка установка стоимости
+        }
+
+        static public void EnterArr(ref Book [] boArr)//метод заполнения массива
+        {
+            for (int i = 0; i < boArr.Length; i++)
+            {
+                Console.Write("Enter author surname:");
+                String strSur = Console.ReadLine();
+                Console.Write("Enter author firstname:");
+                String strFir = Console.ReadLine();
+                Console.Write("Enter title of the book:");
+                String strTit = Console.ReadLine();
+                Console.Write("Enter cost the book(format XX,XX):");
+                float fCost = 0f;
+
+                if (!float.TryParse(Console.ReadLine(), out fCost))
+                {
+                    Console.WriteLine("Error enter cost");
+                }
+                else
+                {
+                    boArr[i] = new Book(new Author(strSur, strFir), strTit, (float)fCost);
+                }
+            }
+        }
+
+        public void ShowBookInfo()//метод вывода информации о книге на консоль
+        {
+            Console.WriteLine(new String('*', 30) + " Book info " + new String('*', 30));
+            Console.WriteLine("Surname: {0}\nFirstname: {1}\nTitle: {2}\nCost: {3}",
+                              this.bookAuthor.strSurname, this.bookAuthor.strFirstname, this.strTitle, this.fCost.ToString());
+            Console.WriteLine(new String('*', 71));
+        }
+
+        //поиск книги по фамилии автора
+        static public void SearchBookByAuthor(String strSearchName, Book[] boArr)
+		{
+			foreach(Book i in boArr)
+			{
+				if(i.bookAuthor.strSurname == strSearchName)
+				{
+					i.ShowBookInfo();
+				}
+            }
+		}
+		
+		static public void SortByTitle(Book[] boArr)//сортировка книг по названию
+		{
+            Array.Sort(boArr, (a, b) => { return String.Compare(a.strTitle, b.strTitle);});//сортировка массива через сравнение строк
+		}
+		
+        //удаление из массива по цене
+		static public void DeletByCost(float fCos, ref Book[] boArr)
+        {
+            //ищем книги с соответствующей ценой
+            for (int i = 0; i < boArr.Length; i++)
+            {
+                if (boArr[i].fCost == fCos)//если находим
+                {
+                    Book [] bTemp = new Book[boArr.Length - 1];//создаём временный массив меньше основного
+                    Array.Copy(boArr, 0, bTemp, 0, i);//копируем элементы до удаляемого
+                    Array.Copy(boArr, i + 1, bTemp, i, boArr.Length - i - 1);//копируем элементы после удаляемого
+                    boArr = bTemp;//основной массив теперь равен временному
+                    i--;//уменьшаем индекс для проверки элемента сдвинувшегося справа на лево
+                }
+            }
+        }
+        
+        static public void ShowArr(Book [] bArr)//показ названий книг всего массива на консоли
+        {
+            foreach(Book i in bArr)
+            {
+                Console.Write(i.strTitle + " ");
+            }
+            Console.WriteLine();
+        }		
+    }//конец class Book
+}//конец namespace DZClassBook
---
+++ Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZClassBook
+{
+    class Program
+    {
+        enum eMenu { searToAuth = 1, sortToTitl, delToCost};
+        static void Main(string[] args)
+        {
+            try
+            {
+                ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+                //////////////////////////////////////// Блок тестирования работоспособности методов //////////////////////////////////////
+                ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+                //тестовый массив объектов класса Book
+                //Book[] bTestArr = { new Book(new Author("A", "a"), "TitA", 11.11f), new Book(new Author("B", "b"), "TitB", 22.22f),
+                //                    new Book(new Author("C", "c"), "TitC", 33.33f), new Book(new Author("D", "d"), "TitD", 44.44f),
+                //                    new Book(new Author("E", "e"), "TitE", 55.55f), new Book(new Author("F", "f"), "TitF", 66.66f),
+                //                    new Book(new Author("G", "g"), "TitG", 77.77f), new Book(new Author("H", "h"), "TitH", 88.88f),
+                //                    new Book(new Author("I", "i"), "TitI", 99.99f), new Book(new Author("J", "j"), "TitJ", 10.10f),};
+
+                //тестовый массив объектов класса Book с переставленнымы элементами для тестирования сортировки по названию (Book.SearchBookToAutho())
+                //Book[] bTestArr2 ={ new Book(new Author("A", "a"), "TitA", 11.11f), new Book(new Author("I", "i"), "TitI", 99.99f),
+                //                    new Book(new Author("E", "e"), "TitE", 55.55f), new Book(new Author("D", "d"), "TitD", 44.44f),
+                //                    new Book(new Author("C", "c"), "TitC", 33.33f), new Book(new Author("F", "f"), "TitF", 66.66f),
+                //                    new Book(new Author("H", "h"), "TitH", 88.88f), new Book(new Author("G", "g"), "TitG", 77.77f),
+                //                    new Book(new Author("B", "b"), "TitB", 22.22f), new Book(new Author("J", "j"), "TitJ", 10.10f),};
+
+
+                //Book.SearchBookByAuthor("H", bTestArr);//проверка работоспособности поиска по фамилии автора
+
+                //проверка работоспособности сортировки по названию
+                //Book.ShowArr(bTestArr2);
+                //Book.SortByTitle(bTestArr2);
+                //Book.ShowArr(bTestArr2);
+
+                //проверка работоспособности удаления по цене
+                //Book.ShowArr(bTestArr);
+                //Book.DeletByCost(55.55f, ref bTestArr);
+                //Book.ShowArr(bTestArr);
+                ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+                int iArrSize = 0;//для ввода раозмера массива
+
+                do
+                {
+
+                    Console.Write("Enter the size of the array of books:");//просим ввести размер массива
+                    iArrSize = Convert.ToInt32(Console.ReadLine());//вводим и преобразовываем ввод
+                    if (iArrSize > 0 && iArrSize < int.MaxValue)//если размер отрицательный или слишком большой просим ввести еще раз
+                        break;
+                } while (true);
+
+                Book[] boArr = new Book[iArrSize];//создаём массив введенного размера
+
+                Book.EnterArr(ref boArr);//заполнение массива
+
+                //начало работы меню
+                do
+                {
+                    //предлагаемые действия
+                    Console.WriteLine(new String('*', 75));
+                    Console.WriteLine("1 - Search by author's name\n2 - Sort by book's title\n3 - Delet by book's cost\nAny other input - Exit ");
+                    Console.Write("Select an action:");
+                    eMenu eSelAct = (eMenu)Convert.ToInt32(Console.ReadLine());//ввод и преобразование выбора действия
+                    switch (eSelAct)
+                    {
+                        //если выброн поиск по имени просим ввести имя и запускаем поиск
+                        case eMenu.searToAuth:
+                            Console.Write("Enter author's name(only Surname):");
+                            Book.SearchBookByAuthor(Console.ReadLine(), boArr);
+                            break;
+                        //если выбрана сортировка запускаем сортировку и показываем результат на консоль
+                        case eMenu.sortToTitl:
+                            Book.SortByTitle(boArr);
+                            Console.WriteLine("Sort List:");
+                            Book.ShowArr(boArr);
+                            break;
+                        //если выбрано удаление по цене просим ввести цену и запускаем метод удаления
+                        case eMenu.delToCost:
+                            Console.Write("Enter book's cost(format XX,XX):");
+                            float fCost = 0f;
+                            if (!float.TryParse(Console.ReadLine(), out fCost))
+                            {
+                                Console.WriteLine("Error enter cost");
+                            }
+                            else
+                            {
+                                Book.DeletByCost(fCost, ref boArr);
+                            }
+                            break;
+                        //при любом другом вводе завершаем программу
+                        default:
+                            return;                            
+                    }
+                } while (true) ;
+            }//конец общего try
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.Read();
+            }
+        }//конец Main
+    }//конец class Program
+}//конец namespace DZClassBook
